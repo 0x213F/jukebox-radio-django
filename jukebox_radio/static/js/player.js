fbb4d2a5cb5f26418f52073df15178b26c991463
Josh Schultheiss <josh@schultheiss.io>
2020-12-08 19:56:05 -0800

    styles

--- jukebox_radio/static/js/player.js
+++ jukebox_radio/static/js/player.js
@@ -184,7 +184,13 @@ const musicSearchSubmit = function() {
           <td>${result.provider}</td>
           <td>${result.name}</td>
           <td>${result.artist_name}</td>
-          <td><button class="btn btn-primary add-to-queue" data-class="${result.class}" data-uuid="${result.uuid}">+</button>
+          <td>
+            <button class="btn btn-primary add-to-queue" data-class="${result.class}" data-uuid="${result.uuid}">
+              <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-plus-square-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg" style="position: relative; top: -2px;">
+                <path fill-rule="evenodd" d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
+              </svg>
+            </button>
+          </td>
         </tr>`
       );
     }
