0ce40874fa9dee179e3e4577d3cde5833c331172
Josh Schultheiss <josh@schultheiss.io>
2020-12-08 16:48:14 -0800

    updates to music admins

--- jukebox_radio/music/admin/collection_admin.py
+++ jukebox_radio/music/admin/collection_admin.py
@@ -6,9 +6,8 @@ from django.apps import apps
 from django.contrib import admin
 from django.contrib.auth import admin as auth_admin
 from django.contrib.auth import get_user_model
+from django.db.models import Exists, OuterRef
 from django.utils.html import mark_safe
-
-from jukebox_radio.music.models.provider import GLOBAL_PROVIDER_CHOICES
 
 User = get_user_model()
 
@@ -46,7 +45,7 @@ class CollectionListingAdminInline(admin.TabularInline):
             name = name[:64] + '...'
 
         return mark_safe(f'<a href="{url}">{name}</a>')
-    list_track_name.short_description = 'TRACK NAME'
+    list_track_name.short_description = 'TRACK'
 
     def list_album_name(self, obj):
         name = obj.track.album_name
@@ -74,7 +73,8 @@ class ProviderListFilter(admin.SimpleListFilter):
     parameter_name = 'provider'
 
     def lookups(self, request, model_admin):
-        return GLOBAL_PROVIDER_CHOICES
+        Collection = apps.get_model("music", "Collection")
+        return Collection.PROVIDER_CHOICES
 
     def queryset(self, request, queryset):
         value = self.value()
@@ -83,11 +83,76 @@ class ProviderListFilter(admin.SimpleListFilter):
         return queryset.filter(provider=value)
 
 
-class BaseCollectionAdmin(admin.ModelAdmin):
+class CollectionFormatListFilter(admin.SimpleListFilter):
+    title = 'format'
+    parameter_name = 'format'
 
-    list_filter = (ProviderListFilter,)
+    def lookups(self, request, model_admin):
+        Collection = apps.get_model("music", "Collection")
+        return Collection.FORMAT_CHOICES
+
+    def queryset(self, request, queryset):
+        value = self.value()
+        if not value:
+            return queryset
+        return queryset.filter(format=value)
+
+
+class CollectionHasRelatedCollectionListingsFilter(admin.SimpleListFilter):
+    title = 'has tracks'
+    parameter_name = 'has_related_collection_listings'
+
+    YES = 'yes'
+    NO = 'no'
+
+    def lookups(self, request, model_admin):
+        return (
+            (True, 'Yes'),
+            (False, 'No'),
+        )
+
+    def queryset(self, request, queryset):
+        CollectionListing = apps.get_model("music", "CollectionListing")
+
+        value = self.value()
+        if not value:
+            return queryset
+
+        if value == 'True':
+            return queryset.filter(
+                Exists(
+                    CollectionListing.objects.filter(
+                        collection_id=OuterRef("uuid"),
+                    )
+                )
+            )
+        elif value == 'False':
+            return queryset.filter(
+                ~Exists(
+                    CollectionListing.objects.filter(
+                        collection_id=OuterRef("uuid"),
+                    )
+                )
+            )
+        else:
+            raise Exception('Invalid choice')
+
+
+@admin.register(apps.get_model("music.Collection"))
+class CollectionAdmin(admin.ModelAdmin):
+
+    list_filter = (ProviderListFilter, CollectionFormatListFilter, CollectionHasRelatedCollectionListingsFilter,)
 
     inlines = (CollectionListingAdminInline,)
+
+    list_display = (
+        'list_uuid',
+        'list_name',
+        'list_artist_name',
+        'list_image',
+        'provider',
+        'format',
+    )
 
     fieldsets = (
         ("CONTENT", {
@@ -124,42 +189,28 @@ class BaseCollectionAdmin(admin.ModelAdmin):
     def list_uuid(self, obj):
         return mark_safe(f'<tt>{obj.uuid}</tt>')
 
+    def list_name(self, obj):
+        name = obj.name
+        if len(name) < 32:
+            return name
+
+        return name[:32] + '...'
+    list_name.short_description = 'NAME'
+
+    def list_artist_name(self, obj):
+        artist_name = obj.artist_name
+        if len(artist_name) < 64:
+            return artist_name
+
+        return artist_name[:64] + '...'
+    list_artist_name.short_description = 'ARTIST NAME'
+
+    def list_image(self, obj):
+        url = obj.img_url or obj.img.url
+        return mark_safe(f'<img src="{url}" style="height: 15px;" />')
+    list_image.short_description = 'IMAGE'
+
     def list_image_large(self, obj):
         url = obj.img_url or obj.img.url
         return mark_safe(f'<img src="{url}" style="height: 256px;" />')
-
-
-@admin.register(apps.get_model("music.Album"))
-class AlbumAdmin(BaseCollectionAdmin):
-
-    search_fields = (
-        "uuid",
-        "name",
-        "artist_name",
-        "external_id",
-    )
-
-    def get_queryset(self, request):
-        Collection = apps.get_model('music', 'Collection')
-
-        qs = super().get_queryset(request)
-        qs = qs.filter(format=Collection.FORMAT_ALBUM)
-        return qs
-
-
-@admin.register(apps.get_model("music.Playlist"))
-class PlaylistAdmin(BaseCollectionAdmin):
-
-    search_fields = (
-        "uuid",
-        "name",
-        "artist_name",
-        "external_id",
-    )
-
-    def get_queryset(self, request):
-        Collection = apps.get_model('music', 'Collection')
-
-        qs = super().get_queryset(request)
-        qs = qs.filter(format=Collection.FORMAT_PLAYLIST)
-        return qs
+    list_image_large.short_description = 'IMAGE'
