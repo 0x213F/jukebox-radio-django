df0268bdff2075852b1556a324184f6a54294f46
Josh <josh@imac.lan>
2020-12-14 00:30:40 -0800

    Update prod settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -11,6 +11,8 @@ from .base import env
 
 # GENERAL
 # ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#debug
+DEBUG = False
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
