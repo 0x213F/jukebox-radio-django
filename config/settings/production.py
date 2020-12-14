4daa4009b01bf052bcabd57f10de5dea22ee469f
Josh <josh@imac.lan>
2020-12-14 00:26:39 -0800

    Update prod settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -14,7 +14,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "jukebox.radio", "jukeboxrad.io"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "jukebox.radio", "jukeboxrad.io", "143.110.229.8"]
 
 # DATABASES
 # ------------------------------------------------------------------------------
