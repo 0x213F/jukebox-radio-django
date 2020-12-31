2dceea7ec171cd4113bfeaa5bcb39dbb9f35bab0
Josh Schultheiss <josh@schultheiss.io>
2020-12-31 00:11:06 -0800

    More updates to API

--- jukebox_radio/music/views/search_view.py
+++ jukebox_radio/music/views/search_view.py
@@ -3,6 +3,13 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 from jukebox_radio.core.base_view import BaseView
 from jukebox_radio.music.const import GLOBAL_PROVIDER_CHOICES
+from jukebox_radio.music.const import GLOBAL_PROVIDER_SPOTIFY
+from jukebox_radio.music.const import GLOBAL_PROVIDER_YOUTUBE
+from jukebox_radio.music.const import GLOBAL_PROVIDER_JUKEBOX_RADIO
+from jukebox_radio.music.const import GLOBAL_FORMAT_TRACK
+from jukebox_radio.music.const import GLOBAL_FORMAT_ALBUM
+from jukebox_radio.music.const import GLOBAL_FORMAT_PLAYLIST
+from jukebox_radio.music.const import GLOBAL_FORMAT_VIDEO
 from jukebox_radio.music.search import get_search_results
 
 
@@ -14,9 +21,25 @@ class MusicSearchView(BaseView, LoginRequiredMixin):
         Track = apps.get_model("music", "Track")
         Collection = apps.get_model("music", "Collection")
 
-        providers = request.GET.get("providers").split(",")
-        formats = request.GET.get("formats").split(",")
         query = request.GET.get("query")
+
+        providers = []
+        if request.GET["providerSpotify"] == "true":
+            providers.append(GLOBAL_PROVIDER_SPOTIFY)
+        if request.GET["providerYouTube"] == "true":
+            providers.append(GLOBAL_PROVIDER_YOUTUBE)
+        if request.GET["providerJukeboxRadio"] == "true":
+            providers.append(GLOBAL_PROVIDER_JUKEBOX_RADIO)
+
+        formats = []
+        if request.GET["formatTrack"] == "true":
+            formats.append(GLOBAL_FORMAT_TRACK)
+        if request.GET["formatAlbum"] == "true":
+            formats.append(GLOBAL_FORMAT_ALBUM)
+        if request.GET["formatPlaylist"] == "true":
+            formats.append(GLOBAL_FORMAT_PLAYLIST)
+        if request.GET["formatVideo"] == "true":
+            formats.append(GLOBAL_FORMAT_VIDEO)
 
         search_results = []
         for (provider_slug, _) in GLOBAL_PROVIDER_CHOICES:
--- jukebox_radio/streams/models/queue_model.py
+++ jukebox_radio/streams/models/queue_model.py
@@ -1,66 +1,131 @@
 import uuid
 
+from django.apps import apps
 from django.db import models
+from django.db import transaction
+from django.db.models import Prefetch
+from django.db.models import F
 
 import pgtrigger
 
 
+class QueueManager(models.Manager):
+
+    @pgtrigger.ignore('streams.Queue:protect_inserts')
+    def create_queue(self, index=None, stream=None, track=None, collection=None, user=None, **kwargs):
+        """
+        Custom create method
+        """
+        Queue = apps.get_model('streams', 'Queue')
+
+        if not index:
+            last_queue = Queue.objects.last_queue(stream)
+            index = last_queue.index + 1
+
+        queue_head = Queue.objects.get_head(stream)
+        if queue_head.index >= index:
+            raise ValueError('Index value is too small')
+
+        # NOTE: This is a little hacky. We calculate the offset that in queue
+        #       indexes need to be bumped up by.
+        _tracks = collection.list_tracks() if collection else [None]
+        if not len(_tracks):
+            raise ValueError(f'Collection has no tracks: {collection.uuid}')
+        offset = len(_tracks)
+
+        with transaction.atomic():
+            up_next_tracks = Queue.objects.up_next_tracks(stream)
+            up_next_tracks.update(index=F('index') + offset)
+
+            queues = []
+
+            parent_queue = Queue(
+                stream=stream,
+                index=(index + offset),
+                user=user,
+                collection=collection,
+                is_abstract=True,
+            ) if collection else None
+            if parent_queue:
+                queues.append(parent_queue)
+
+            for _track in _tracks:
+                queue = Queue(
+                    stream=stream,
+                    index=index,
+                    user=user,
+                    track=(_track or track),
+                    collection=collection,
+                    is_abstract=False,
+                    parent=parent_queue,
+                )
+                queues.append(queue)
+                index += 1
+
+            Queue.objects.bulk_create(queues)
+
+
 class QueueQuerySet(models.QuerySet):
-    def in_stream(self, stream):
-        """
-        Get a list of all the queue items not yet played in a stream. Only root
-        nodes of the queue tree are returned.
-        """
-        queue_qs = self.filter(
+
+    def last_queue(self, stream):
+        return self.filter(stream=stream, deleted_at__isnull=True).order_by('-index')[0]
+
+    def get_head(self, stream):
+        return Queue.objects.get(stream=stream, is_head=True, deleted_at__isnull=True)
+
+    def up_next_tracks(self, stream):
+        queue_head = Queue.objects.get_head(stream)
+        if not queue_head:
+            return Queue.objects.none()
+        return self.filter(
+            index__gt=queue_head.index,
             stream=stream,
-            played_at__isnull=True,
-            deleted_at__isnull=True,
             is_abstract=False,
+            deleted_at__isnull=True,
+        ).order_by('index')
+
+    def up_next(self, stream):
+        queue_head = Queue.objects.get_head(stream)
+        if not queue_head:
+            return Queue.objects.none()
+
+        return (
+            self
+            .prefetch_related(
+                Prefetch(
+                    'children',
+                    queryset=(
+                        Queue.objects.filter(
+                            index__gt=queue_head.index,
+                            deleted_at__isnull=True,
+                        ).order_by('index')
+                    ),
+                    to_attr='ordered_children',
+                )
+            )
+            .filter(
+                index__gt=queue_head.index,
+                stream=stream,
+                parent__isnull=True,
+                deleted_at__isnull=True,
+            ).order_by('index')
         )
-
-        queue_list = list(queue_qs)
-        if not queue_list:
-            return []
-
-        # Fun stuff here. We need to iron out our queryset into a sorted list.
-        # Since this is a doubly-linked-list, I don't think it is possible to
-        # do this inside the Django ORM. This sorts in O(N^2)
-        sorted_queue_list = [queue_list[0]]
-        del queue_list[0]
-
-        while queue_list:
-            initial_len = len(queue_list)
-
-            for idx in range(len(queue_list)):
-                queue = queue_list[idx]
-
-                if queue == sorted_queue_list[-1].next_queue_ptr:
-                    sorted_queue_list.append(queue)
-                    del queue_list[idx]
-                    break
-
-                if queue == sorted_queue_list[0].prev_queue_ptr:
-                    sorted_queue_list.insert(0, queue)
-                    del queue_list[idx]
-                    break
-
-            if initial_len == len(queue_list):
-                raise Exception("Infinite loop detected!")
-
-        return sorted_queue_list
 
 
 @pgtrigger.register(
     pgtrigger.Protect(name="protect_deletes", operation=pgtrigger.Delete)
+)
+@pgtrigger.register(
+    pgtrigger.Protect(name="protect_inserts", operation=pgtrigger.Insert)
 )
 class Queue(models.Model):
     '''
     A queue is a piece of content (either a track or collection) selected to
     play in a stream. There are two important data structures used here.
 
-      1: LINKED LIST - This is responsible for the order in which tracks are
-                       played. Each of these is a child node of the tree
-                       explained in 2: and points to a track, not a collection.
+      1: LIST        - This is responsible for the order in which tracks are
+                       played. The list of queues for a stream are ordered by
+                       the index field.
       2: TREE        - This is responsbile for the tree like structure to each
                        queued item. For example, when I queue up an album
                        (collection) it creates a queue item for the album and
@@ -74,10 +139,19 @@ class Queue(models.Model):
     application logic, queue tree depth is limited to a 1. In the future, a
     recursive tree structure with unlimited depth is possible.
     '''
+    INITIAL_INDEX = 1
 
-    objects = QueueQuerySet.as_manager()
+    objects = QueueManager.from_queryset(QueueQuerySet)()
 
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    stream = models.ForeignKey(
+        "streams.Stream",
+        related_name="+",
+        on_delete=models.CASCADE,
+    )
+
+    index = models.IntegerField(null=True, blank=True)
 
     user = models.ForeignKey("users.User", on_delete=models.CASCADE)
 
@@ -97,34 +171,12 @@ class Queue(models.Model):
     start_at = models.PositiveIntegerField(null=True, blank=True)
     end_at = models.PositiveIntegerField(null=True, blank=True)
 
-    play_with_stem_separation = models.BooleanField(default=False)
-    play_bass_stem = models.BooleanField(null=True, blank=True)
-    play_drums_stem = models.BooleanField(null=True, blank=True)
-    play_vocals_stem = models.BooleanField(null=True, blank=True)
-    play_other_stem = models.BooleanField(null=True, blank=True)
-
-    stream = models.ForeignKey(
-        "streams.Stream",
-        related_name="+",
-        on_delete=models.CASCADE,
-    )
-    prev_queue_ptr = models.ForeignKey(
-        "streams.Queue",
-        related_name="+",
-        on_delete=models.CASCADE,
-        null=True,
-    )
-    next_queue_ptr = models.ForeignKey(
-        "streams.Queue",
-        related_name="+",
-        on_delete=models.CASCADE,
-        null=True,
-    )
+    # stem_separation = models.ArrayField(CharField)
 
     is_abstract = models.BooleanField()
-    parent_queue_ptr = models.ForeignKey(
+    parent = models.ForeignKey(
         "streams.Queue",
-        related_name="+",
+        related_name="children",
         on_delete=models.CASCADE,
         null=True,
     )
