f27f9cd6533d77a0af71f55aacc482fb2d1a1a05
Josh Schultheiss <josh@schultheiss.io>
2021-01-13 17:41:05 -0800

    Remove unused import statements

--- jukebox_radio/users/views/get_settings_view.py
+++ jukebox_radio/users/views/get_settings_view.py
@@ -1,16 +1,7 @@
-from cryptography.fernet import Fernet
-
-from django.apps import apps
-from django.conf import settings
-from django.contrib import messages
-from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.sites.shortcuts import get_current_site
-from django.urls import reverse
 
 from jukebox_radio.core.base_view import BaseView
 from jukebox_radio.core.utils import generate_spotify_authorization_uri
-from jukebox_radio.networking.actions import make_request
 
 User = get_user_model()
 
