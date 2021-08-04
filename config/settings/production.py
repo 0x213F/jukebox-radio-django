7dadc03829fc99e2d5ccd9eba3ab92d3533df37f
phyllisbarba <josh@schultheiss.io>
2021-08-04 16:26:37 -0700

    Allowed hosts: all (for local development)

--- config/settings/production.py
+++ config/settings/production.py
@@ -15,13 +15,14 @@ DEBUG = False
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = [
-    "localhost",
-    "0.0.0.0",
-    "127.0.0.1",
-    "prod.jukeboxrad.io",
-    "143.110.229.8",
-]
+ALLOWED_HOSTS = ['*']
+# [
+#     "localhost",
+#     "0.0.0.0",
+#     "127.0.0.1",
+#     "prod.jukeboxrad.io",
+#     "143.110.229.8",
+# ]
 
 SITE_URL = "https://jukeboxrad.io/"
 
